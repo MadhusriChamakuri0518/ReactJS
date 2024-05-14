bd8c4dbeabdbe34cd38afe561eb063f1f88639c2
MadhusriChamakuri0518 <madhusrichamakuri18@gmail.com>
2024-05-14 23:07:56 +0530

    added some additional content to fragments file

--- src/components/Fragments/Columns.js
+++ src/components/Fragments/Columns.js
@@ -6,6 +6,11 @@ function Columns() {
         <td>Name</td>
         <td>Madhusri</td>
     </React.Fragment>
+
+    // we can also use the shorter syntax as 
+    // <>
+    // but can't use the attribute "key" inside the React fragment like this <React.Fragment key={}></React.Fragment>
+    // </>
   )
 }
 
